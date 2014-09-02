a0cfdc833f6d62fb5053a770e427c0e255da8904
Roberto Guerra <uris77@gmail.com>
2014-09-02 17:53:05 -0600

    Using MongoClient for mongo connection.

--- src/main/groovy/org/pasmo/persistence/MongoDBClient.groovy
+++ src/main/groovy/org/pasmo/persistence/MongoDBClient.groovy
@@ -3,6 +3,9 @@ package org.pasmo.persistence
 import com.mongodb.DB
 import com.mongodb.DBCollection
 import com.mongodb.Mongo
+import com.mongodb.MongoClient
+import com.mongodb.MongoCredential
+import com.mongodb.ServerAddress
 
 class MongoDBClient {
     private final String DB_HOST = System.getProperty("USER_DB_HOST")
@@ -11,18 +14,19 @@ class MongoDBClient {
     private final String USERNAME = System.getProperty("MONGO_USERNAME")
     private final String PASSWORD =  System.getProperty("MONGO_PASSWORD")
 
-    private Mongo _mongoClient
+    private MongoClient _mongoClient
 
-    Mongo getMongoClient() {
+    MongoClient getMongoClient() {
         if(_mongoClient == null) {
-            _mongoClient = new Mongo(DB_HOST, DB_PORT)
-
+            _mongoClient = new MongoClient(new ServerAddress(DB_HOST, DB_PORT),
+                    [MongoCredential.createMongoCRCredential(USERNAME, DB_NAME, PASSWORD as char[])])
         }
         _mongoClient
     }
 
     DB getDatabase() {
         if(USERNAME && PASSWORD) {
+
             mongoClient.getDB(DB_NAME).authenticateCommand(USERNAME, PASSWORD as char[])
         }
         mongoClient.getDB(DB_NAME)
