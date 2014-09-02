3ca55469a2a6db234f4c138d17a2d5b53079af1c
Roberto Guerra <uris77@gmail.com>
2014-09-02 14:24:26 -0600

    Refactord DB_PORT

--- src/main/groovy/org/pasmo/persistence/MongoDBClient.groovy
+++ src/main/groovy/org/pasmo/persistence/MongoDBClient.groovy
@@ -6,16 +6,14 @@ import com.mongodb.Mongo
 
 class MongoDBClient {
     private final String DB_HOST = System.getProperty("USER_DB_HOST")
-    def DB_PORT = System.getProperty("USER_DB_PORT") //Integer.parseInt(System.getProperty("USER_DB_PORT"))
+    private final int DB_PORT = Integer.parseInt(System.getProperty("USER_DB_PORT"))
     private final String DB_NAME = System.getProperty("USER_DB_NAME")
 
     private Mongo _mongoClient
 
     Mongo getMongoClient() {
         if(_mongoClient == null) {
-            println "\n\nDB_PORT: ${System.getProperty('USER_DB_PORT')}"
-            println "DB_NAME: ${System.getProperty('USER_DB_NAME')}"
-            _mongoClient = new Mongo(DB_HOST, Integer.parseInt(DB_PORT.toString()))
+            _mongoClient = new Mongo(DB_HOST, DB_PORT)
         }
         _mongoClient
     }
