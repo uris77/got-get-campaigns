f46d829aaf9fc7eda87ee501a9337b7c22edeef8
Roberto Guerra <uris77@gmail.com>
2014-09-02 17:27:40 -0600

    Added authentication to mongo connection

--- src/main/groovy/org/pasmo/persistence/MongoDBClient.groovy
+++ src/main/groovy/org/pasmo/persistence/MongoDBClient.groovy
@@ -8,17 +8,23 @@ class MongoDBClient {
     private final String DB_HOST = System.getProperty("USER_DB_HOST")
     private final int DB_PORT = Integer.parseInt(System.getProperty("USER_DB_PORT"))
     private final String DB_NAME = System.getProperty("USER_DB_NAME")
+    private final String USERNAME = System.getProperty("MONGO_USERNAME")
+    private final String PASSWORD =  System.getProperty("MONGO_PASSWORD")
 
     private Mongo _mongoClient
 
     Mongo getMongoClient() {
         if(_mongoClient == null) {
             _mongoClient = new Mongo(DB_HOST, DB_PORT)
+
         }
         _mongoClient
     }
 
     DB getDatabase() {
+        if(USERNAME && PASSWORD) {
+            return mongoClient.getDB(DB_NAME).authenticate(USERNAME, PASSWORD as char[])
+        }
         mongoClient.getDB(DB_NAME)
     }
 
