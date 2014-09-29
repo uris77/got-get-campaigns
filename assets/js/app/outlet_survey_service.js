4e1dcdc6bdc13662efcbb7358de2c36baa17e11c
Roberto Guerra <uris77@gmail.com>
2014-09-29 08:10:46 -0600

    Created an Outlet Survey Service

---
+++ assets/js/app/outlet_survey_service.js
@@ -0,0 +1,18 @@
+( function (ng) {
+
+    function OutletSurveyService($http) {
+        return {
+            fetchSurveys: function(survey_id) {
+                return $http.get("/api/outletSurveys/" + survey_id + "/" + "hotspot");
+            },
+
+            createSurvey: function(surveyId, params) {
+                return $http.post("/api/outletSurveys/" + surveyId, params);
+            }
+        }
+    }
+
+    ng.module("OutletSurveyService", [])
+        .factory("OutletSurveyService", ["$http", OutletSurveyService])
+
+})(angular)
--- assets/js/app/pasmo_crud_app.js
+++ assets/js/app/pasmo_crud_app.js
@@ -1,9 +1,11 @@
 var PasmoApp = { apiUrls: {} };
 
 angular.module("PasmoApp",
-	["ui.router", "url_utils",
-        "surveys", "PasmoLocation",
-        "TraditionaOutletSurvey","NonTraditionalOutletSurvey", "Hotspots"])
+	[
+        "ui.router", "url_utils", "OutletSurveyService",
+        "surveys", "PasmoLocation","TraditionaOutletSurvey",
+        "NonTraditionalOutletSurvey", "Hotspots"
+    ])
 	.run(["$rootScope", "$http", "$state", function($rootScope, $http, $state){
 		console.info("Starting Pasmo App......");
 	}]);
